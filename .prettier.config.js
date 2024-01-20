000ddabd3a5d880a05dfef0577ef996ed60d672a
mcreekmore <matthewacreekmore@gmail.com>
2024-01-20 15:22:45 -0500

    added some projects and a little description

---
+++ .prettier.config.js
@@ -0,0 +1,9 @@
+const config = {
+  singleQuote: true,
+  semi: false,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  plugins: ['prettier-plugin-astro'],
+}
+
+export default config
--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,12 +1,14 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 const projectSchema = z.object({
-  repo: z.string(),
-  link: z.string(),
+  name: z.string(),
+  link: z.string().optional(),
+  linkText: z.string().optional(),
   description: z.string(),
-});
+  repoLink: z.string(),
+})
 
-export type Project = z.infer<typeof projectSchema>;
+export type Project = z.infer<typeof projectSchema>
 
 // export async function getProjects(
 //   number: 1 | 2 | 3 | 4 | 5 | 6,
@@ -23,34 +25,40 @@ export type Project = z.infer<typeof projectSchema>;
 export async function getProjects(): Promise<Project[]> {
   return [
     {
-      repo: "create-t3-app",
-      link: "https://github.com/nexxeln/create-t3-app",
-      description: "The best way to start a full-stack, typesafe Next.js app",
+      name: 'Moves',
+      repoLink: 'https://github.com/mcreekmore/moves',
+      link: 'https://v1.creekmore.io/#/moves',
+      linkText: 'See more',
+      description: 'A cross-paltform Flutter app for local event tracking',
     },
     {
-      repo: "nexxel.dev",
-      link: "https://github.com/nexxeln/nexxel.dev",
-      description: "Boring personal site built with Astro",
+      name: 'creekmore.io v1',
+      repoLink: 'https://github.com/mcreekmore/app',
+      link: 'https://v1.creekmore.io/',
+      linkText: 'Visit',
+      description:
+        'Version 1 of my personal website. Built with the NEVM stack',
     },
     {
-      repo: "license-generator",
-      link: "https://github.com/nexxeln/license-generator",
-      description: "Create licenses for your projects right from your terminal",
+      name: 'ammobroker',
+      repoLink: 'https://github.com/mcreekmore/ammobroker',
+      // link: "https://github.com/nexxeln/license-generator",
+      description: 'Fork of streetmerchant for finding ammo instead of gpus',
     },
-    {
-      repo: "spotify-voice-control",
-      link: "https://github.com/nexxeln/spotify-voice-control",
-      description: "Voice control for Spotify through the terminal",
-    },
-    {
-      repo: "hackernews",
-      link: "https://github.com/nexxeln/hackernews",
-      description: "Hacker News clone made with SolidStart and tRPC",
-    },
-    {
-      repo: "dots",
-      link: "https://github.com/nexxeln/dots",
-      description: "My dotfiles for MacOS",
-    },
-  ];
+    // {
+    //   repo: "spotify-voice-control",
+    //   link: "https://github.com/nexxeln/spotify-voice-control",
+    //   description: "Voice control for Spotify through the terminal",
+    // },
+    // {
+    //   repo: "hackernews",
+    //   link: "https://github.com/nexxeln/hackernews",
+    //   description: "Hacker News clone made with SolidStart and tRPC",
+    // },
+    // {
+    //   repo: "dots",
+    //   link: "https://github.com/nexxeln/dots",
+    //   description: "My dotfiles for MacOS",
+    // },
+  ]
 }
