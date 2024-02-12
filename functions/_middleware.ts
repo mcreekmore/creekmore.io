131f2b339ffe6bfc917b44bd99d5a5fe57b5b747
mcreekmore <matthewacreekmore@gmail.com>
2024-02-12 09:43:30 -0500

    add cloudflare middleware function enable CORS

---
+++ functions/_middleware.ts
@@ -0,0 +1,12 @@
+// Respond to OPTIONS method
+export const onRequestOptions: PagesFunction = async () => {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      'Access-Control-Max-Age': '86400',
+    },
+  });
+};
