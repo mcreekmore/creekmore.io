d319795e932c2a54b4095391ba13de90694c2187
mcreekmore <matthewacreekmore@gmail.com>
2025-03-17 12:29:07 -0400

    remove caching for script routes

--- src/pages/win.ts
+++ src/pages/win.ts
@@ -3,14 +3,20 @@ import type { APIRoute } from 'astro';
 export const GET: APIRoute = async () => {
   const githubRawUrl = 'https://raw.githubusercontent.com/mcreekmore/dotfiles/main/windows/remote_setup.ps1';
 
-  const response = await fetch(githubRawUrl);
+  const response = await fetch(githubRawUrl, {
+    cache: 'no-store',
+    headers: {
+      'Cache-Control': 'no-cache'
+    }
+  });
+
   const script = await response.text();
 
   return new Response(script, {
     headers: {
       'Content-Type': 'text/plain',
       'Content-Disposition': 'inline',
-      'Cache-Control': 'no-cache',
+      'Cache-Control': 'no-cache, max-age=0',
     },
   });
 };
