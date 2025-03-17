e78e2ad9dc295c21091f74c9af0d820d15b6d486
mcreekmore <matthewacreekmore@gmail.com>
2025-03-17 10:46:44 -0400

    add setup route

---
+++ src/pages/setup.ts
@@ -0,0 +1,17 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = async () => {
+  const githubRawUrl = 'https://raw.githubusercontent.com/mcreekmore/dotfiles/main/scripts/executable_setup.sh';
+
+  const response = await fetch(githubRawUrl);
+  const script = await response.text();
+
+  return new Response(script, {
+    headers: {
+      'Content-Type': 'text/plain',
+      'Content-Disposition': 'inline',
+      'Cache-Control': 'no-cache',
+    },
+  });
+};
+
